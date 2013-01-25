eabb14fcda80b522ced474220095430500a06bcd
Tyler Hedrick <tylerhedrick@me.com>
2013-01-25 18:26:29 -0500

    Initial Commit

---
+++ Mundi/MAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAAppDelegate.h
+//  Mundi
+//
+//  Created by Tyler Hedrick on 1/25/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mundi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mundi
+//
+//  Created by Tyler Hedrick on 1/25/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAAppDelegate class]));
+    }
+}
