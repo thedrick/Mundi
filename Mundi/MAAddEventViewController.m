48bb006144a68a2b37ef28b5421dd6f3ef85aa31
Mitra Raman <mitraraman@gmail.com>
2013-01-26 00:43:11 -0500

    event creation 2

---
+++ Mundi/MAAddEventViewController.m
@@ -0,0 +1,38 @@
+//
+//  MAAddEventViewController.m
+//  Mundi
+//
+//  Created by Mitra Raman on 1/25/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import "MAAddEventViewController.h"
+
+@interface MAAddEventViewController ()
+
+@end
+
+@implementation MAAddEventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Mundi/MAEventCreation2ViewController.h
@@ -0,0 +1,16 @@
+//
+//  MAEventCreation2ViewController.h
+//  Mundi
+//
+//  Created by Mitra Raman on 1/26/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAEventCreation2ViewController : UIViewController
+- (IBAction)publicOrPrivate:(id)sender;
+- (IBAction)invite:(id)sender;
+- (IBAction)inviteView:(id)sender;
+
+@end
