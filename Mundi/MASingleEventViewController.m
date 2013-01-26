3b1ede861bf8038767b01c1bdc31a8ccb393e8b3
Tyler Hedrick <tylerhedrick@me.com>
2013-01-25 22:34:19 -0500

    commiting to pull. Might delete my profile views

---
+++ Mundi/MASingleEventViewController.m
@@ -0,0 +1,38 @@
+//
+//  MASingleEventViewController.m
+//  Mundi
+//
+//  Created by Tyler Hedrick on 1/25/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import "MASingleEventViewController.h"
+
+@interface MASingleEventViewController ()
+
+@end
+
+@implementation MASingleEventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
